61429d80fa5fe0a97d1aaeb529b761b2ce19fb98
UnfaltringFaith <deniska45652@gmail.com>
2025-07-08 00:06:01 +0300

    Начало проекта

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-3xl font-bold underline bg-amber-50"> Hello world! </h1>
+
+    </div>
+</body>
+
+</html>
